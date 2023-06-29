090af6fdd17e602ed273774d82ef2e50a37cfee7
Mattheus <mattheusp382@gmail.com>
2023-06-29 14:55:40 -0300

    first_commit

---
+++ scripts.js
@@ -0,0 +1,7 @@
+// var el = document.querySelector('fa-solid fa-bars menu-mobile-icon');
+
+// el.addEventListener('click',()=>{
+    // Queremos mostrar o menu.
+    // var menuItems = document.querySelector('.menu-items');
+    // menuItems.classList.add('show');
+// });
